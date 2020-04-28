744862241b28c4e3e4f51d674c4bc7b296043295
David Möller <davidm@student.chalmers.se>
2020-04-28 12:54:39 +0200

    Updated application

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "Hello there, this is a test application for Kubernetes!")
+		c.JSON(http.StatusOK, "Hello there, this is a test application for Kubernetes! "+
+			"This application has been changed in the GitHub master branch, built in Docker Hub and then updated automatically by Kubernetes!")
 	})
 
 	r.Run(":8080")
