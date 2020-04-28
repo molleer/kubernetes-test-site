c2a7c0bcba971f62377da1da40772bbbc299416c
David Möller <damoller99@hotmail.se>
2020-04-28 12:15:05 +0200

    Created test application and production Dockerfile

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "Hello there, this is a test application for Kubernetes!")
+	})
+
+	r.Run(":8080")
+}
