15bf8e3f5f6aee1f4e28daa8bb1e9a1a81b5c0fa
David Möller <davidm@student.chalmers.se>
2020-09-03 19:16:01 +0200

    Changed message

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "This is a test site running on nginx. The the application rebuilds when master is updated on github. This is really cool and its really fast! It also uses github actions")
+		c.JSON(http.StatusOK, "This is a test site running on nginx. The the application rebuilds when master is updated on github. This is really cool and its really fast!\n"+
+			"This repo has also been used to debug molleer/blame-alert!")
 	})
 
 	r.Run(":8080")
